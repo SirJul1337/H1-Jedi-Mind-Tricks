6e414ebe4c6ff0fd2052f2780c15ee587a0c00f4
SirJul1337 <niklas.jul@julit.dk>
2022-03-29 11:54:03 +0200

    Add project files.

---
+++ Jedi Mind Tricks/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Jedi_Mind_Tricks
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Dictionary<string, int> characters = new Dictionary<string, int>()
+            {
+            { "Luke", 24 },
+            { "Han", 17 },
+            { "Chewbacca", 500 }
+            };
+
+            characters.Add("Niklas", 18);
+            
+            if (characters.Remove("Han"))
+            {
+                Console.WriteLine("Han has been removed");
+            }
+            else
+            {
+                Console.WriteLine("Couldnt find Han");
+            }
+
+
+            foreach(var character in characters)
+            {
+                Console.WriteLine(character);
+            }
+        }
+    }
+}
+
